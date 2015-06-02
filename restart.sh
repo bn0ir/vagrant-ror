a9bdb576d279640249241fba054ccbca4daa5180
blacknoir <blacknoir@blacknoir>
2015-06-02 15:01:37 +0500

    Add private key for vagrant user if config/private.key exists

--- restart.sh
+++ restart.sh
@@ -1,3 +1,8 @@
 cat /vagrant/config/env.global > /etc/profile.d/appenv.sh
+
+if [ /vagrant/config/private.key ]; then
+    rsync /vagrant/config/private.key /home/vagrant/.ssh/id_rsa
+fi
+
 source /etc/profile
 service nginx restart
